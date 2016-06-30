7c63332cb050c01b8aaca9363e53a02697786548
armandoM55 <league@Mac-mini-3.local>
2016-06-30 16:42:40 -0700

    level1
    level juan

---
+++ src/Eagle.java
@@ -0,0 +1,19 @@
+
+public class Eagle {
+	String featherColors = " ";
+	int Age;
+
+	void fly() {
+		System.out.println("flying");
+	}
+
+	void Haunt() {
+		System.out.println("Haunt");
+	}
+	Eagle (String FeatherColors,int Age)
+	{
+		this.featherColors= "brown";
+		
+			this.Age= 15;
+	}
+}
---
+++ src/SpiritAnimal.java
@@ -0,0 +1,9 @@
+
+public class SpiritAnimal {
+public static void main(String[] args) {
+	
+	Eagle Alex= new Eagle("Brown",15);
+	Alex.fly();
+	Alex.Haunt();
+	}
+}
