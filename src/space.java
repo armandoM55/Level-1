39ffe65c116517963ef74729cdddf53ffc3ec909
armandoM55 <armando.moscaira@e3civichigh.com>
2017-06-15 17:00:31 -0700

    Space

--- src/space.java
+++ src/space.java
@@ -1,21 +1,28 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class space {
+public class space  {
 		JFrame frame;
+	
 		JPanel panel=new JPanel();
 		final int Width=500;
 	final int Height=800;
+	
 public static void main(String[] args) {
 	
-	
+space niBBa=new space();	
+GamePanel alien;
 }
 space(){
 	frame = new JFrame();	
 	//panel=new
 setup();	
+GamePanel alien=new GamePanel();
+
 }
+
 void setup(){
+	
 	frame.pack();
 	frame.setVisible(true);
 	
